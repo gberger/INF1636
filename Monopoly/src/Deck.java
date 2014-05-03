9999301614b9a417b96ce8d5295b4684a950255b
Guilherme Berger <guilherme.berger@gmail.com>
2014-05-02 23:52:52 -0300

    add Deck#getCards

--- Monopoly/src/Deck.java
+++ Monopoly/src/Deck.java
@@ -4,11 +4,15 @@ import java.util.List;
 abstract public class Deck<T> {
   protected List<T> cards;
   
-  public T get(int idx){
+  public T get(int idx) {
     return cards.get(idx);
   }
   
-  public int size(){
+  public List<T> getCards() {   
+    return this.cards;
+  }
+  
+  public int size() {
     return cards.size();
   }
   
