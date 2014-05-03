4d6e01258e38c48105bacce93901c839e140f227
Felipe Luiz <ferocha@globo.com>
2014-05-03 00:20:41 -0300

    Added 'this' token to implicit calls

--- Monopoly/src/UI.java
+++ Monopoly/src/UI.java
@@ -5,9 +5,9 @@ import ui.*;
 public class UI extends JFrame {
   public UI() {
     this.setTitle("Monopoly");
-    this.setSize(500, 500);
+    this.setSize(600, 600);
     this.setVisible(true);
-    setDefaultCloseOperation( EXIT_ON_CLOSE );
+    this.setDefaultCloseOperation( EXIT_ON_CLOSE );
     
     BoardPanel board = new BoardPanel();
     this.getContentPane().add(board);
