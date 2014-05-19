ffbfc528d9ad88634cbdfea4c4ac30ccfe1bc0e8
Guilherme Berger <guilherme.berger@gmail.com>
2014-05-18 21:09:57 -0300

    refactor BoardPanel

--- Monopoly/src/ui/BoardPanel.java
+++ Monopoly/src/ui/BoardPanel.java
@@ -4,94 +4,93 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-
-import org.json.simple.JSONObject;
 
 import engine.Player;
 import engine.PlayerColor;
 
 public class BoardPanel extends JPanel {
+  private static final long serialVersionUID = -6420464876258642413L;
   private final int width = 600;
   private final int height = 600;
-  
-  private BufferedImage board;
+
   private List<Player> players;
-  private JSONObject pinImages = new JSONObject();
-  
-  
-  public BoardPanel(List<Player> players) {
-    this.players = players;
-    BufferedImage pin = null;
-    this.setBounds(350,40,this.width,this.height);
-    try {
-      board = ImageIO.read(new File("img/board.jpg"));
-    } catch (IOException ex) {
-      System.out.println("Unable to load board image!");
-    }
-    
-    for (PlayerColor color : PlayerColor.values()) {
-      try {
-        pin = ImageIO.read(new File("img/"+color.toString().toLowerCase()+"_pin.png"));
-      } catch (IOException ex) {
-        System.out.println("Unable to load pin image!");
-      }
-      pinImages.put(color.toString(), pin);
-    }
-    
+  private BufferedImage boardImage;
+  private Map<PlayerColor, BufferedImage> pinImages = new HashMap<PlayerColor, BufferedImage>();
+
+  private void initializeBoardImage() throws IOException {
+    boardImage = ImageIO.read(new File("img/board.jpg"));
   }
-  
-  protected JSONObject getPlaceCoordinates(int placeId)
-  {
-    JSONObject coord = new JSONObject();
+
+  private void initializePinImages() throws IOException {
+    for (PlayerColor color : PlayerColor.values()) {
+      BufferedImage pin = ImageIO.read(new File("img/" + color.toString().toLowerCase() + "_pin.png"));
+      pinImages.put(color, pin);
+    }
+  }
+
+  public BoardPanel(List<Player> players) {
+    this.setBounds(350,40,this.width,this.height);
+
+    this.players = players;
+
+    try {
+      this.initializeBoardImage();
+      this.initializePinImages();
+    } catch (IOException e) {
+      System.out.println("Unable to load images.");
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  protected Map<String, Integer> getCoordinatesForPositionAndColor(int position, PlayerColor color) {
+    Map<String, Integer> coords = new HashMap<String, Integer>();
     int x, y;
-    
-    if(placeId < 10) {
-      x = board.getWidth()-80;
-      y = 90*(placeId) + 90;
-    } else if (placeId < 20) {
-      x = board.getWidth()-120-((placeId-10)*90);
-      y = board.getHeight()-90;
-    } else if (placeId < 30) {
+
+    if(position < 10) {
+      x = boardImage.getWidth()-80;
+      y = 90*(position) + 90;
+    } else if (position < 20) {
+      x = boardImage.getWidth()-120-((position-10)*90);
+      y = boardImage.getHeight()-90;
+    } else if (position < 30) {
       x = 70;
-      y = board.getHeight()-100-((placeId-20)*90);
+      y = boardImage.getHeight()-100-((position-20)*90);
     } else {
-      x = 90+(placeId-30)*90;
+      x = 90+(position-30)*90;
       y = 65;
     }
-    x *= (float)this.width/board.getWidth();
-    y *= (float)this.height/board.getHeight();
-    coord.put("x", x);
-    coord.put("y", y);
-    return coord;
+    x *= (float)this.width/boardImage.getWidth();
+    y *= (float)this.height/boardImage.getHeight();
+
+    x -= color.ordinal() * 5;
+
+    coords.put("x", x);
+    coords.put("y", y);
+
+    return coords;
   }
-  
+
+  protected Map<String, Integer> getCoordinatesForPlayer(Player player) {
+    return this.getCoordinatesForPositionAndColor(player.getPosition(), player.getColor());
+  }
+
   @Override
   protected void paintComponent(Graphics g) {
-      BufferedImage pin = null;
-      //super.paintComponent(g);
-      this.setBounds(350,40,this.width,this.height);
-      
-      g.drawImage(board, 0, 0,this.width,this.height, null);
-      
-      
-      int[] playerCountByPlace = new int[40];
-      
-      for(int i=0;i<this.players.size();i++) {
-        Player current = players.get(i);
-        
-        pin = (BufferedImage)pinImages.get(current.getColor().toString());
-        
-        JSONObject coord = this.getPlaceCoordinates(current.getPosition());
-        playerCountByPlace[current.getPosition()]++;
-        g.drawImage(pin, (int)coord.get("x")-(playerCountByPlace[current.getPosition()]*5), (int)coord.get("y"),15,24, null);
-      }
-      
-      
-      
+    this.setBounds(350, 40, this.width, this.height);
+
+    g.drawImage(boardImage, 0, 0, this.width, this.height, null);
+
+    for(Player player : players){
+      BufferedImage pin = pinImages.get(player.getColor());
+      Map<String, Integer> coords = this.getCoordinatesForPlayer(player);
+      g.drawImage(pin, coords.get("x"), coords.get("y"), 15, 24, null);
+    }
   }
-  
 }
