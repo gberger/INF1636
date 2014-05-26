4cfdf7701372b7783f0669e5f3c02a1861e39c4d
Guilherme Berger <guilherme.berger@gmail.com>
2014-05-25 22:54:10 -0300

    message when going to jail

--- Monopoly/src/com/monopoly/engine/squares/GoToJailSquare.java
+++ Monopoly/src/com/monopoly/engine/squares/GoToJailSquare.java
@@ -4,6 +4,7 @@ import org.json.simple.JSONObject;
 
 import com.monopoly.engine.core.Game;
 import com.monopoly.engine.core.Player;
+import com.monopoly.ui.UserInterface;
 
 public class GoToJailSquare extends Square {
   public GoToJailSquare(JSONObject jobj, Game game) {
@@ -13,7 +14,9 @@ public class GoToJailSquare extends Square {
 
   @Override
   public void affectLandingPlayer(Player player) {
+    UserInterface ui = this.game.getUI();
     player.goToJail();
+    ui.showMessage(player.getName() + " vai para a cadeia...");
   }
 
   @Override
